9d6797f6d4b403b0cf8f4a76962eaadef574516f
saitcantunc <tuncsaitcan@gmail.com>
2022-01-24 08:08:38 +0300

    Blog and CTA Done

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,17 @@
 import "./Footer.css";
 
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className='gpt3__footer section__padding'>
+			<div className='gpt3__footer-heading'>
+				<h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+			</div>
+
+			<div className='gpt3__footer-btn'>
+				<p>Request Early Access</p>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
